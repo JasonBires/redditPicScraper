ebb826270c2aaa28c7d42b8c2ab8f16c9022f7d5
Jason Bires <jason.m.bires@gmail.com>
2014-10-28 00:35:38 -0400

    Can now do multiple pages

--- mainDL.py
+++ mainDL.py
@@ -2,34 +2,43 @@ import re
 import os
 import mechanize
 
-br = mechanize.Browser()
-linkChaser = mechanize.Browser()
+"""Pass in a URL from imgur, ergo imgLink.url
+   Return a properly formatted string"""
+def nameImgurFile(imgUrl):
+    name = imgUrl.lstrip('http://')
+    name = name.replace('/', '_')
+    name = name.replace('?', '')
+    return name
 
-br.addheaders = [('User-agent', 'earthpornScraping')]
-linkChaser.addheaders = [('User-agent', 'imgurScrapin')]
+def pullPage(pageString):
+    br = mechanize.Browser()
+    linkChaser = mechanize.Browser()
 
-br.open("http://www.reddit.com/r/earthporn/")
-files = []
+    br.addheaders = [('User-agent', 'earthpornScraping')]
+    linkChaser.addheaders = [('User-agent', 'imgurScrapin')]
 
-for link in br.links(url_regex=r"http://(.)*imgur(.)*"):
-    if link.text != "[IMG]":
-        if not re.match("(.)*i.imgur(.)*", link.url):
-            print "FOLLOWING:", link.url
-            linkChaser.open(url=link.url)
-            print "followed the link"
-            for imgLink in linkChaser.links(url_regex=r"(.)*/i.imgur(.)*"):
-                fileName = imgLink.url.lstrip('http://')
-                fileName = fileName.replace('/', '_')
-                #print dir
-                print imgLink.base_url
-                print imgLink.url
-                print fileName
-                #fileName = os.path.join(dir, fileName.replace('/', '_'))
-                linkChaser.retrieve("http:" + imgLink.url, filename=fileName)
-            print "pumped it off my chest"
-        else:
-            print link
-            linkChaser.open(url=link.url)
-            fileName = link.url.lstrip('http://')
-            fileName = fileName.replace('/', '_')
-            br.retrieve(link.url, filename=fileName)
+    br.open(pageString)
+    files = []
+
+    for link in br.links(url_regex=r"http://(.)*imgur(.)*"):
+        if link.text != "[IMG]":
+            if not re.match("(.)*i.imgur(.)*", link.url):
+                linkChaser.open(url=link.url)
+                for imgLink in linkChaser.links(url_regex=r"(.)*/i.imgur(.)*"):
+                    fileName = nameImgurFile(imgLink.url)
+                    linkChaser.retrieve("http:" + imgLink.url, filename=fileName)
+            else:
+                linkChaser.open(url=link.url)
+                fileName = nameImgurFile(link.url)
+                br.retrieve(link.url, filename=fileName)
+def pullXPages(numPages, pageString):
+    curPage = pageString
+    br = mechanize.Browser()
+    br.addheaders = [('User-agent', 'earthpornScraping')]
+    for x in range(0, numPages):
+        pullPage(curPage)
+        br.open(curPage)
+        curPage = br.links(url_regex=r"http://www.reddit.com/r/(.)*?count(.)*&after=(.)*").next().url
+        print curPage
+        
+pullXPages(5, "http://www.reddit.com/r/earthporn")
